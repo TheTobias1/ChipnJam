b9c355b6d1cba2bbb2c29c3bf24f8ce48d3925ba
Tobias_Desktop <tobyhendricks1@gmail.com>
2020-05-15 21:06:48 +0100

    New repo

---
+++ Assets/TobiasStuff/Scripts/Weapon System/Projectile.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody body;
+
+    public float projectileWidth;
+    public float projectileSpeed;
+    public float projectileTimeout;
+
+    public bool damageOnOverlap;
+    public bool damageOnCollision;
+
+    public LayerMask mask;
+    public List<string> damagingTags;
+
+    public GameObject deathPrefab;
+
+    public void Start()
+    {
+        Invoke("Kill", projectileTimeout);
+    }
+
+    public void SetVelocity(Vector3 force)
+    {
+        body.velocity = force;
+    }
+
+    private void Update()
+    {
+        if(damageOnCollision)
+        {
+            if(CollisionCheck().collider != null)
+            {
+                //Do damage
+
+
+                //Destroy
+                Kill();
+            }
+        }
+
+        if(damageOnOverlap)
+        {
+            Collider[] hits = Overlap();
+
+            foreach(Collider h in hits)
+            {
+                //Do damage
+            }
+        }
+
+
+
+    }
+
+    RaycastHit CollisionCheck()
+    {
+        RaycastHit hit;
+
+        if(projectileWidth > 0.1)
+        {
+            if(Physics.SphereCast(transform.position, projectileWidth, transform.forward, out hit, projectileSpeed * Time.deltaTime + 1, mask))
+            {
+                return hit;
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, projectileSpeed * Time.deltaTime + 1, mask))
+            {
+                return hit;
+            }
+        }
+
+        return new RaycastHit();
+    }
+
+    Collider[] Overlap()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, projectileWidth, mask);
+        List<Collider> goodHits = new List<Collider>();
+
+        foreach(Collider h in hits)
+        {
+            if(damagingTags.Contains(h.tag))
+            {
+                goodHits.Add(h);
+            }
+        }
+
+        return goodHits.ToArray();
+    }
+
+    public void Kill()
+    {
+        CancelInvoke("Kill");
+
+        if(deathPrefab != null)
+        {
+            Instantiate(deathPrefab, transform.position, transform.rotation);
+        }
+
+        Destroy(gameObject);
+    }
+}
