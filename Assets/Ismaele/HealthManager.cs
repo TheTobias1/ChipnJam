b72ac6b1f1f36d3d172dceef0afb37aca124a7f5
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2020-05-15 22:11:01 +0200

    Added health-management system

---
+++ Assets/Ismaele/HealthManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int Health { get; private set; }
+    public int maxHealth = 100;
+
+    public float HealthPercent { get => Health / maxHealth; }
+    public bool IsAlive { get => Health > 0; }
+
+    private void Start()
+    {
+        Health = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (damageAmount <= 0)
+            return;
+
+        if (IsAlive)
+        {
+            Health -= damageAmount;
+            Health = Mathf.Clamp(Health, 0, maxHealth);
+        }
+    }
+
+    public void HealDamage(int healAmount)
+    {
+        if (healAmount <= 0)
+            return;
+
+        if (IsAlive)
+        {
+            Health += healAmount;
+            Health = Mathf.Clamp(Health, 0, maxHealth);
+        }
+    }
+
+    public void Revive(int reviveAmount)
+    {
+        if (reviveAmount <= 0)
+            return;
+
+        if (!IsAlive)
+        {
+            Health = reviveAmount;
+            Health = Mathf.Clamp(Health, 0, maxHealth);
+        }
+    }
+}
