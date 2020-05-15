649e6eb32f5220c76551ab836574e955a47eb070
Tobias_Desktop <tobyhendricks1@gmail.com>
2020-05-15 20:08:44 +0100

    Started movement

---
+++ ChipnJam/Assets/TobiasStuff/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static PlayerInput inputBuffer;
+
+    public static void SetPlayerInput(PlayerInput input)
+    {
+        inputBuffer = input;
+    }
+
+    public static PlayerInput GetPlayerInput()
+    {
+        return InputManager.inputBuffer;
+    }
+
+    private void Update()
+    {
+        InputManager.inputBuffer = PollInput();
+    }
+
+    protected virtual PlayerInput PollInput()
+    {
+        PlayerInput input = new PlayerInput();
+
+        input.moveInput =  new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        input.jump = Input.GetButton("Jump");
+        input.attack = Input.GetButton("Fire1");
+        input.ability = Input.GetButtonDown("Fire2");
+
+        return input;
+    }
+
+
+}
+
+
+public struct PlayerInput
+{
+    public Vector2 moveInput;
+    public bool jump;
+    public bool attack;
+    public bool ability;
+}
---
+++ ChipnJam/Assets/TobiasStuff/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Components
+    private CharacterController controller;
+    private Transform playerModel;
+
+    //Movement attributes
+    public float speed;
+    public float acceleration;
+    public float deceleration;
+    public float gravity;
+    public float jumpForce;
+
+    //Movement State
+    private Vector3 velocity;
+    private float nextJump;
+    private Vector3 forwardDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput inputBuffer = InputManager.GetPlayerInput();
+
+        Move(inputBuffer);
+    }
+
+    protected virtual void Move(PlayerInput input)
+    {
+        Vector2 planearVelocity = CondenseVector3(velocity);
+        Vector2 moveInput = input.moveInput;
+
+        if(moveInput.magnitude > 0.25f)
+        {
+            planearVelocity += moveInput * acceleration * Time.deltaTime;
+            planearVelocity = Vector2.ClampMagnitude(planearVelocity, speed);
+            forwardDirection = velocity;
+        }
+        else
+        {
+            float curSpeed = velocity.magnitude;
+            curSpeed = Mathf.Max(0, curSpeed - deceleration * Time.deltaTime);
+        }
+
+        if(controller.isGrounded)
+        {
+            velocity.y = -0.1f;
+
+            if(input.jump && nextJump < Time.time)
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            velocity.y -= gravity * Time.deltaTime;
+
+            if (input.jump && nextJump < Time.time)
+            {
+                //Jump ability
+            }
+        }
+
+
+    }
+
+    protected virtual void Jump()
+    {
+        nextJump = Time.time + 0.2f;
+
+        velocity.y = jumpForce;
+    }
+
+    //Utility Functions
+    public Vector2 CondenseVector3(Vector3 v)
+    {
+        return new Vector2(v.x, v.z);
+    }
+
+
+}
+
