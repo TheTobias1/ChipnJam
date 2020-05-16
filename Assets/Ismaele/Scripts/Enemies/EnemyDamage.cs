94c05596c97368f6983853799c90248d6d82e466
Tobias_Desktop <tobyhendricks1@gmail.com>
2020-05-16 17:28:26 +0100

    New controller mapping

--- Assets/Ismaele/Scripts/Enemies/EnemyDamage.cs
+++ Assets/Ismaele/Scripts/Enemies/EnemyDamage.cs
@@ -8,6 +8,7 @@ public class EnemyDamage : MonoBehaviour
     private BoxCollider hitBox;
 
     public int damageAmount;
+    public bool damageEnemies;
 
     private void Start()
     {
@@ -16,7 +17,7 @@ public class EnemyDamage : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" || (damageEnemies && other.tag == "Enemy"))
         {
             HealthManager playerHealth = other.GetComponent<HealthManager>();
             playerHealth.TakeDamage(damageAmount);
