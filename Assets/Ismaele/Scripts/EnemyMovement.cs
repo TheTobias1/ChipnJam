ec82b962d83b33beb15ceb53b52c946eb4181ead
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2020-05-16 13:38:35 +0200

    Made enemy sensor and fixed enemy movement

--- Assets/Ismaele/Scripts/EnemyMovement.cs
+++ Assets/Ismaele/Scripts/EnemyMovement.cs
@@ -6,29 +6,20 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(SphereCollider))]
 public class EnemyMovement : PlayerMovement
 {
     // Other properties belonging to the enemy
     private NavMeshAgent enemyAgent;
-    private SphereCollider sensingZone;
-
-    // Determine the current state of the enemy
-    private bool sensesPlayer;
-    private Transform player;
+    public EnemySensor sensor;
 
     protected override void Start()
     {
         base.Start();
 
         enemyAgent = GetComponent<NavMeshAgent>();
-        sensingZone = GetComponent<SphereCollider>();
 
         enemyAgent.updatePosition = false;
         enemyAgent.updateRotation = false;
-
-        sensingZone.radius = 4.5f;
-        sensingZone.isTrigger = true;
     }
 
     protected override void Update()
@@ -40,33 +31,11 @@ public class EnemyMovement : PlayerMovement
         RotateModel();
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            sensesPlayer = true;
-            player = other.transform;
-
-            sensingZone.radius = 7.5f;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            sensesPlayer = false;
-            player = null;
-
-            sensingZone.radius = 4.5f;
-        }
-    }
-
     private PlayerInput ResolveInput()
     {
         PlayerInput enemyInput = new PlayerInput();
 
-        if (!sensesPlayer)
+        if (!sensor.SensesPlayer)
         {
             enemyInput.moveInput = Vector2.zero;
             enemyInput.jump = true;
@@ -75,9 +44,10 @@ public class EnemyMovement : PlayerMovement
         } else
         {
             // Moving towards the player
-            enemyAgent.SetDestination(player.position);
+            enemyAgent.SetDestination(sensor.Player.position);
 
-            enemyInput.moveInput = enemyAgent.desiredVelocity.normalized;
+            enemyInput.moveInput = CondenseVector3(enemyAgent.desiredVelocity.normalized);
+            //enemyInput.moveInput = enemyAgent.desiredVelocity;
             enemyInput.jump = false;
             enemyInput.attack = false;
             enemyInput.ability = false;
